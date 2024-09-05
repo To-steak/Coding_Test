e62a79649234c90501a989f5a9174569ea02bc35
To-steak <70811061+To-steak@users.noreply.github.com>
2024-09-05 14:43:45 +0900

    Update Q10809.cs

--- Q10809.cs
+++ Q10809.cs
@@ -14,3 +14,16 @@ for (int i = 0; i < S.Length; i++)
     }
 }
 Console.WriteLine(string.Join(" ", result));
+
+/*
+string? S = Console.ReadLine();
+if (S == null) S = "";
+IEnumerable<int> temp = Enumerable.Repeat(-1, 26);
+int[] result = temp.ToArray();
+for (int i = 0; i < S.Length; i++)
+{
+    var index = S[i] - 'a';
+    if (result[index] == -1) result[index] = i;
+}
+Console.WriteLine(string.Join(" ", result));
+*/
