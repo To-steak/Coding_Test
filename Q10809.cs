5c4865d1df6dc3d86b0c4728b11eb8da585ac39e
To-steak <70811061+To-steak@users.noreply.github.com>
2024-09-05 14:37:00 +0900

    Update Q10809.cs

--- Q10809.cs
+++ Q10809.cs
@@ -1,42 +1,16 @@
 ﻿string? S = Console.ReadLine();
 if (S == null) S = "";
-
-int index = 0;
-bool flag =  true;
-char[] alphabet = new char[26];
-int[] result = new int[26];
-
-for (var i = 'a'; i <= 'z'; i++) alphabet[index++] = i;
-index = 0;
-
-for (int i = 0; i < alphabet.Length; i++)
+int[] result = new int[]
 {
-    for (int j = 0; j < S.Length; j++)
-    {
-        if(alphabet[i].Equals(S[j]))
-        {
-            result[index] = j;
-            flag = false;
-            break;
-        }
-    }
-
-    if(flag)
-    {
-        result[index] = -1;
-    }
-    flag = true;
-    index++;
-}
-
-for (int k = 0; k < result.Length; k++)
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+for (int i = 0; i < S.Length; i++)
 {
-    if(k < result.Length - 1)
+    var index = S[i] - 'a';
+    if(index >= 0 && index <= 25)
     {
-        Console.Write(result[k] + " ");
-    }
-    else
-    {
-        Console.Write(result[k]);
+        if(result[index] == -1)
+            result[index] = i;
     }
 }
+Console.WriteLine(string.Join(" ", result));
